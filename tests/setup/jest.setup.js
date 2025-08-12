11332e4b4262013b91ce133bf034c72418396944
Shikhar Verma <shikhar1verma@gmail.com>
2025-08-12 22:22:04 +0530

    Resolve issue in a test case for setup.

--- tests/setup/jest.setup.js
+++ tests/setup/jest.setup.js
@@ -6,9 +6,20 @@ jest.mock('next/image', () => ({ __esModule: true, default: (props) => {
   return <img {...props} />;
 }}));
 
-// Silence framer-motion warnings in tests
-jest.mock('framer-motion', () => ({
-  __esModule: true,
-  motion: new Proxy({}, { get: () => (props) => <div {...props} /> }),
-  AnimatePresence: ({ children }) => <div>{children}</div>,
-})); 
+// Silence framer-motion warnings in tests and support refs on motion.*
+jest.mock('framer-motion', () => {
+  const React = require('react');
+  const motion = new Proxy({}, {
+    get: () =>
+      React.forwardRef((props, ref) => {
+        const { children, ...rest } = props || {};
+        return (
+          <div ref={ref} {...rest}>
+            {children}
+          </div>
+        );
+      }),
+  });
+  const AnimatePresence = ({ children }) => <>{children}</>;
+  return { __esModule: true, motion, AnimatePresence };
+}); 
